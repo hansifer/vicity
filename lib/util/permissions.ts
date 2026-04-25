b3704948344e7ad7bde71b2eee16f7b8157e0fd5
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-25 11:23:39 -0600

    add permission util

--- lib/util/permissions.ts
+++ lib/util/permissions.ts
@@ -2,17 +2,16 @@ import { getExceptionText } from '@/lib/util/logging';
 
 export async function getNavigatorPermission(
   name: PermissionName,
-): Promise<PermissionState> {
+): Promise<PermissionState | 'unsupported'> {
   if (!navigator.permissions) {
-    return 'prompt';
+    return 'unsupported';
   }
 
   try {
     return (await navigator.permissions.query({ name })).state;
   } catch (ex) {
-    console.error(
-      getExceptionText(ex, `Error checking navigator permission "${name}"`),
-    );
-    return 'prompt';
+    console.warn(getExceptionText(ex, `Error checking permission "${name}"`));
+
+    return 'unsupported';
   }
 }
