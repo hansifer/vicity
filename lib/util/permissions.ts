2e949af943bcab827b4671f3414737ba80bc39ba
Hans Meyer <hansifer@users.noreply.github.com>
2026-04-26 10:37:49 -0600

    add permissions util

--- lib/util/permissions.ts
+++ lib/util/permissions.ts
@@ -11,7 +11,37 @@ export async function getNavigatorPermission(
     return (await navigator.permissions.query({ name })).state;
   } catch (ex) {
     console.warn(getExceptionText(ex, `Error checking permission "${name}"`));
-
-    return 'unsupported';
   }
+
+  return 'unsupported';
+}
+
+// returns unregister function or null if onchange is not supported
+export async function permissionOnchange(
+  name: PermissionName,
+  callback: (state: PermissionState) => void,
+): Promise<(() => void) | null> {
+  if (!navigator.permissions) {
+    return null;
+  }
+
+  try {
+    const status = await navigator.permissions.query({ name });
+
+    // some browsers may support onchange but not have it defined until it's accessed
+    const _ = status.onchange;
+
+    // todo: is this the best way to detect support for onchange?
+    if ('onchange' in status) {
+      status.onchange = (e) => callback((e.target as PermissionStatus).state);
+
+      return () => {
+        status.onchange = null;
+      };
+    }
+  } catch (ex) {
+    console.warn(getExceptionText(ex, `Error setting onchange for "${name}"`));
+  }
+
+  return null;
 }
